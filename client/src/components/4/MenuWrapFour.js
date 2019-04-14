a6fbbba4722454357ca024cc2ed22fae8576c6ad
unknown <kevinchoinj@yahoo.com>
2019-04-14 19:03:56 -0400

    moved react app into client directory, removed menu 4, refactor of menu1 css to use transforms instead of position values

--- client/src/components/4/MenuWrapFour.js
+++ client/src/components/4/MenuWrapFour.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import MenuButtonFour from './MenuButtonFour';
-import MenuPanelFour from './MenuPanelFour';
 
 class MenuWrapFour extends React.Component{
   render(){
     return(
       <div>
-        <MenuButtonFour />
-        <MenuPanelFour />
+
       </div>
     );
   }
